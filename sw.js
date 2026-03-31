61e9568bf5e6bcb63ae2ccac48653071e74bd8dc
Bozhidar456 <boji.m.dar@gmail.com>
2026-03-31 13:30:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,14 +3,15 @@ const assets = [
   './',
   './index.html',
   './manifest.json',
+  './sw.js',
   './box.png',
   './coin.png',
   './protivnic1.png',
   './shrek.png',
-  './truba.png',
-  './sw.js'
+  './truba.png'
 ];
 
+// Записване на файловете в кеша
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
@@ -19,6 +20,19 @@ self.addEventListener('install', e => {
   );
 });
 
+// Активиране и изчистване на стар кеш
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Работа офлайн
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => {
